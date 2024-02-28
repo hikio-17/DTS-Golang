352f2aa58468c623d8684afe6d0d666c1de3437e
hikio-17 <fajri010217@gmail.com>
2024-02-28 20:35:41 +0700

    add: sesi - 1 (Data types, Constant & Operators)

--- sesi 1/project-pertama/main.go
+++ sesi 1/project-pertama/main.go
@@ -17,6 +17,12 @@ func main() {
 
 	fmt.Println("nama saya adalah ==>", name)
 	fmt.Println("Umur saya ==>", age)
-
 	fmt.Printf("Nama saya adalah %s dan umur saya saat ini %d", name, age)
+
+	// Pengecekan tipe data
+	fmt.Printf("Nama jenis tipe data nya adalah %\t \n", name)
+
+	// nilai constants
+	const fullName string = "Fajri Muhammad Tio"
+	fmt.Printf("Hello %s", fullName)
 }
